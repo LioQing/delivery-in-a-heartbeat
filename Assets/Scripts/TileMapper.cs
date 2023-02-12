7987cd2811981c680cd4b33120e8289fa07964c5
LioQing <46854695+LioQing@users.noreply.github.com>
2023-02-12 15:26:32 +0800

    Revert

--- Assets/Scripts/TileMapper.cs
+++ Assets/Scripts/TileMapper.cs
@@ -12,17 +12,6 @@ public class TileMapper : MonoBehaviour
     public Vector3 exitPosition;
     public int nextMap;
 
-<<<<<<< Updated upstream
-=======
-    private void CreateFoodObj(int x, int y)
-    {
-        int rndNo = Random.Range(0, 6);
-        var foodSprite = foodSprites[rndNo]; // Chooses a sprite from foodSprites at random
-        var foodObject = Instantiate(foodSprite, transform);
-        foodObject.transform.position = new Vector3(x, y);
-    }
-
->>>>>>> Stashed changes
     private void Awake()
     {
         var fullPath = $"{Application.streamingAssetsPath}/{mapFilePath}";
@@ -42,42 +31,14 @@ public class TileMapper : MonoBehaviour
         {
             lineData = (lines[y].Trim()).Split(' ');
             if (lineData.Length == 0 || lineData[0] == "-1")
-=======
-        int mapEnd = 0; // Probably not a good fix. Open to improvements  -Eric
-
-        for (int y = 0; y < lines.Length; y++)
-        {
-            var lineData = (lines[y].Trim()).Split(' ');
-            if (lineData.Length == 0)
->>>>>>> Stashed changes
                 break;
 
             for (int x = 0; x < lineData.Length; x++)
             {
-<<<<<<< Updated upstream
                 var tileIndex = int.Parse(lineData[x]);
                 var tileSprite = tileSprites[tileIndex];
                 var tileObject = Instantiate(tileSprite, transform);
                 tileObject.transform.position = new Vector3(x, -y);
-=======
-                for (int x = 0; x < lineData.Length; x++) // Read tile map line
-                {
-                    var tileIndex = int.Parse(lineData[x]);
-                    var tileSprite = tileSprites[tileIndex];
-                    var tileObject = Instantiate(tileSprite, transform);
-                    tileObject.transform.position = new Vector3(x, -y);
-                }
-            }
-            else if (mapEnd == 1 && lineData is not null && lineData.Length != 0)
-            {
-                if (lineData[0] == "0") // Initialize food positions
-                {
-                    Debug.Log("Instatiated food, x: " + int.Parse(lineData[1]) + ", y: {0}" + int.Parse(lineData[2]) + " loop: " + y);
-                    CreateFoodObj(int.Parse(lineData[1]), -int.Parse(lineData[2]));
-                }
-                else
-                    Debug.Log("Unknown item id. Loop: " + y);
->>>>>>> Stashed changes
             }
         }
 
